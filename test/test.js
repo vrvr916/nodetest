95205a9233be8e933a901d9ba61338b4f5e947c0
ramvenkata <ramvenkata@ebay.com>
2015-10-12 15:44:44 -0700

    updating package

---
+++ test/test.js
@@ -0,0 +1,24 @@
+var assert = require("assert");
+describe('Array', function() {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
+
+
+
+var chai = require('chai');
+var assert = chai.assert;
+var foo = "bar";
+var tea = {'flavors':[23,32,55]};
+describe('hello',function(){
+
+assert.typeOf(foo, 'string');
+assert.equal(foo, 'bar');
+assert.lengthOf(foo, 3)
+assert.property(tea, 'flavors');
+assert.lengthOf(tea.flavors, 3);
+});
